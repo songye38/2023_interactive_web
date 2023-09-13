1bfa7ead630cd8e7a451fd02980f839abba278b0
songye38 <ddev0308@gmail.com>
2023-09-14 01:04:42 +0900

    add props

---
+++ react-tictactoe-app/src/components/Square.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Square.css"
+
+export default class Square extends React.Component {
+    render(){
+        return (
+            <button className="square" onClick={()=>{
+                console.log("click")
+            }}>
+                {this.props.value}
+            </button>
+        )
+    }
+}
