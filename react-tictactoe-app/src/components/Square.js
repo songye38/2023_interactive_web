f7908b06456ef081029ddc83bb6877feaaaca346
songye38 <ddev0308@gmail.com>
2023-09-14 01:17:34 +0900

    add constructor

--- react-tictactoe-app/src/components/Square.js
+++ react-tictactoe-app/src/components/Square.js
@@ -2,12 +2,19 @@ import React from "react"
 import "./Square.css"
 
 export default class Square extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            value : null,
+        }
+    }
     render(){
         return (
             <button className="square" onClick={()=>{
-                console.log("click")
+                this.setState({value:'X'}) //state변경해주는 부분
             }}>
-                {this.props.value}
+                {this.state.value} //변경해준 값을 칸 안에 표시해주기
             </button>
         )
     }
