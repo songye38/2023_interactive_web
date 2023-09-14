95aece39cb77639ae3ec75acaf840842f42fc874
songye38 <ddev0308@gmail.com>
2023-09-14 14:32:19 +0900

    class componenet to function component using hook

--- react-tictactoe-app/src/components/Square.js
+++ react-tictactoe-app/src/components/Square.js
@@ -1,15 +1,12 @@
 import React from "react"
 import "./Square.css"
 
-export default class Square extends React.Component {
-
-    render(){
+const Square=({onClick,value})=> {
         return (
-            <button className="square" onClick={()=>{
-                // this.setState({value:'X'}) //state변경해주는 부분
-            }}>
-                {this.props.value} 
+            <button className="square" onClick={onClick}>
+                {value} 
             </button>
         )
-    }
 }
+
+export default Square
